4632f034cbe6c6b86a4a4a889705252408817d01
Andrew Sytchev <berryknoedel@gmail.com>
2022-05-20 12:15:39 +0300

    Add upload_photo_to_vk_server function

--- fetch_vk.py
+++ fetch_vk.py
@@ -28,23 +28,33 @@ def fetch_vk_wall_upload_server(access_token, group_id):
     return decoded_response
 
 
-def save_vk_wall_photo(access_token, group_id, album_id, upload_url, user_id, file_name):
-    with open('C:\\Users\\berry\\Desktop\\UrokiGit\\VKComicsPosting\\'+file_name, 'rb') as file:
-        url = 'https://api.vk.com/method/photos.saveWallPhoto'
-        payload = {
-            'group_id': group_id,
-            'album_id': album_id,
-            'upload_url': upload_url,
-            'user_id': user_id,
-            'access_token': access_token,
-            'v': '5.131'
-        }
+def upload_photo_to_vk_server(upload_url, file_name):
+    with open(file_name, 'rb') as file:
         files = {
             'photo': file
         }
-        response = requests.post(url, params=payload, files=files)
+        response = requests.post(upload_url, files=files)
         response.raise_for_status()
         decoded_response = response.json()
         if 'error' in response.json():
             raise requests.exceptions.HTTPError(decoded_response["error"])
+    return decoded_response
+
+
+def save_vk_wall_photo(access_token, user_id, group_id, photo, server, vk_hash):
+    url = 'https://api.vk.com/method/photos.saveWallPhoto'
+    payload = {
+            'group_id': group_id,
+            'user_id': user_id,
+            'hash': vk_hash,
+            'photo': photo,
+            'server': server,
+            'access_token': access_token,
+            'v': '5.131'
+    }
+    response = requests.post(url, params=payload)
+    response.raise_for_status()
+    decoded_response = response.json()
+    if 'error' in response.json():
+        raise requests.exceptions.HTTPError(decoded_response["error"])
     return decoded_response
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from dotenv import load_dotenv
 from fetch_xkcd import fetch_comics_xkcd_metadadata
-from fetch_vk import fetch_vk_groups, fetch_vk_wall_upload_server, save_vk_wall_photo
+from fetch_vk import upload_photo_to_vk_server, fetch_vk_wall_upload_server, save_vk_wall_photo
 from data_collection import download_picture
 import os
 
 
 def main():
     load_dotenv()
-    
+
     xkcd_comic_metadata = fetch_comics_xkcd_metadadata(
         'https://xkcd.com/info.0.json'
     )
@@ -19,21 +19,21 @@ def main():
         'C:/Users/berry/Desktop/UrokiGit/VKComicsPosting'
     )
     file_name = img_url.rsplit('/', 1)[-1]
-    print (file_name)
 
     vk_access_token = os.getenv('ACCESS_TOKEN')
     group_id = os.getenv('GROUP_ID')
     vk_server_data = fetch_vk_wall_upload_server(vk_access_token, group_id)
-    album_id = abs(vk_server_data['response']['album_id'])
     upload_url = vk_server_data['response']['upload_url']
     user_id = vk_server_data['response']['user_id']
+    vk_server_answer = upload_photo_to_vk_server(upload_url, file_name)
+
     print(save_vk_wall_photo(
         vk_access_token,
-        group_id,
-        album_id,
-        upload_url,
         user_id,
-        file_name
+        group_id,
+        vk_server_answer['photo'],
+        vk_server_answer['server'],
+        vk_server_answer['hash']
         )
     )
 
