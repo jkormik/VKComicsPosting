6ce522741d0bda6091ae41d72a1721de1effe40c
Andrew Sytchev <berryknoedel@gmail.com>
2022-05-27 14:23:32 +0300

    Make code dry
    Delete unnecessary response checks.

--- fetch_xkcd.py
+++ fetch_xkcd.py
@@ -5,8 +5,6 @@ def fetch_xkcd_comics_metadadata(url):
     response = requests.get(url)
     response.raise_for_status()
     decoded_response = response.json()
-    if 'error' in decoded_response:
-        raise requests.exceptions.HTTPError(decoded_response["error"])
     return decoded_response
 
 
@@ -14,6 +12,4 @@ def fetch_last_xkcd_comics_num():
     response = requests.get('https://xkcd.com/info.0.json')
     response.raise_for_status()
     decoded_response = response.json()
-    if 'error' in decoded_response:
-        raise requests.exceptions.HTTPError(decoded_response["error"])
     return decoded_response['num']
