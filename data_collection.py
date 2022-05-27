694b3a973c1ac467fd5d21b649533e381fd91b70
Andrew Sytchev <berryknoedel@gmail.com>
2022-05-27 14:18:01 +0300

    Add response status check

--- data_collection.py
+++ data_collection.py
@@ -9,5 +9,6 @@ def download_picture(picture_url, picture_path, **params):
     splited_path = os.path.split(parsed.path)
     filename = splited_path[-1]
     response = requests.get(picture_url, params=params)
+    response.raise_for_status()
     with open(f"{picture_path}/{filename}", "wb") as file:
         file.write(response.content)
