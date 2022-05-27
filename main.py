7c61ccfe62fd2b9138c5a7eb5b6840d1ddc955a9
Andrew Sytchev <berryknoedel@gmail.com>
2022-05-27 20:54:00 +0300

    Add temporary file deletion in any case

--- main.py
+++ main.py
@@ -18,47 +18,45 @@ def main():
         random_xkcd_comics_number
     )
 
-    img_url = xkcd_comic_metadata['img']
-
-    download_picture(
-        img_url,
-        os.getcwd()
-    )
-
-    file_name = get_file_name_from_url(img_url)
-
     vk_access_token = os.getenv('ACCESS_TOKEN')
     group_id = os.getenv('GROUP_ID')
     vk_server_data = fetch_vk_wall_upload_server(vk_access_token, group_id)
     upload_url = vk_server_data['response']['upload_url']
     user_id = vk_server_data['response']['user_id']
-    vk_server_answer = upload_photo_to_vk_server(upload_url, file_name)
 
-    os.remove(file_name)
+    img_url = xkcd_comic_metadata['img']
+    file_name = get_file_name_from_url(img_url)
+    try:
+        download_picture(
+            img_url,
+            os.getcwd()
+        )
+        vk_server_answer = upload_photo_to_vk_server(upload_url, file_name)
+        vk_wall_upload_server_response = save_vk_wall_photo(
+            vk_access_token,
+            user_id,
+            group_id,
+            vk_server_answer['photo'],
+            vk_server_answer['server'],
+            vk_server_answer['hash']
+        )
 
-    vk_wall_upload_server_response = save_vk_wall_photo(
-        vk_access_token,
-        user_id,
-        group_id,
-        vk_server_answer['photo'],
-        vk_server_answer['server'],
-        vk_server_answer['hash']
-    )
+        owner_id = f"-{group_id}"
+        from_group = 1
+        own_id = vk_wall_upload_server_response['response'][0]['owner_id']
+        media_id = vk_wall_upload_server_response['response'][0]['id']
+        attachments = f"photo{own_id}_{media_id}"
+        message = xkcd_comic_metadata['alt']
 
-    owner_id = f"-{group_id}"
-    from_group = 1
-    own_id = vk_wall_upload_server_response['response'][0]['owner_id']
-    media_id = vk_wall_upload_server_response['response'][0]['id']
-    attachments = f"photo{own_id}_{media_id}"
-    message = xkcd_comic_metadata['alt']
-
-    post_vk_wall(
-        vk_access_token,
-        owner_id,
-        from_group,
-        message,
-        attachments
-    )
+        post_vk_wall(
+            vk_access_token,
+            owner_id,
+            from_group,
+            message,
+            attachments
+        )
+    finally:
+        os.remove(file_name)
 
 
 if __name__ == "__main__":
